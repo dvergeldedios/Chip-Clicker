698cffa481729d3ac5c7d42e96ccda9d8ce3f6bc
RHuangcode <ryan.huang26@ycdsbk12.ca>
2025-02-20 11:00:59 -0500

    Added Main menu button methods
    Needs actual buttons to be added in unity for function

---
+++ Scripts/Test.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Test.cs:MonoBehaviour {
+
+    public void PlayGame()
+    {
+        // Replace "GameScene" with the name of your actual game scene.
+        SceneManager.LoadScene("GameScene");
+    }
+
+    // Called by the Quit button
+    public void QuitGame()
+    {
+        // Note: This will only quit the application when built. In the editor, it does nothing.
+        Application.Quit();
+    }
+
+    // Called by the Settings button
+    public void OpenSettings()
+    {
+        // Replace "SettingsScene" with your settings scene name, or implement settings UI here.
+        SceneManager.LoadScene("SettingsScene");
+    }
+
+    // Called by the Shop button
+    public void OpenShop()
+    {
+        // Replace "ShopScene" with your shop scene name, or implement shop UI here.
+        SceneManager.LoadScene("ShopScene");
+    }
+}
